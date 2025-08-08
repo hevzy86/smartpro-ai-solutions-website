e450f565ac6b53d16739ee3df1b8cf7d4b51621d
hevzy86 <hevzy86@gmail.com>
2025-08-07 22:30:27 -0700

    fix: update booking button text from 'Book my Cal' to 'Book my Free Call'

--- src/app/components/FloatingBookButton.tsx
+++ src/app/components/FloatingBookButton.tsx
@@ -15,7 +15,7 @@ export default function FloatingBookButton({ onClick }: FloatingBookButtonProps)
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
         </svg>
       </div>
-      <div className="font-semibold leading-5 antialiased">Book my Cal</div>
+      <div className="font-semibold leading-5 antialiased">Book my Free Call</div>
     </button>
   );
 }
